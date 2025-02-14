1a0b12d4820163116876faaa2b4e13fc606a93e9
Ivann <i.herrera.535614@umindanao.edu.ph>
2025-02-14 08:40:58 +0800

    Optimized Coliders

--- src/Scene/Game.ts
+++ src/Scene/Game.ts
@@ -27,7 +27,7 @@ export class Game extends Phaser.Scene {
   ball!: Ball
   board!: Phaser.GameObjects.Image
 
-  DIRECTION = 1
+  // DIRECTION = 1
   TIME_LIMIT = 5
 
   upMotion!: Phaser.Math.Vector2
@@ -50,6 +50,8 @@ export class Game extends Phaser.Scene {
     this.ball.setVelocity(SPEED)
     this.ball.setMaxVelocity(700)
 
+    this.ball.setBounce(1,1)
+
     this.ballMotion = this.physics.add.image(this.ball.x, this.ball.y, "ballMotion")
     this.ballMotion.setDisplayOrigin(this.ballMotion.width, 0)
     this.ballMotion.setAlpha(0.3)
@@ -57,10 +59,10 @@ export class Game extends Phaser.Scene {
 
     this.player_paddle = this.physics.add.image(50, 300, 'player').setImmovable()
 
-    this.b_wall = this.physics.add.staticImage(480, 625, 'player').setAlpha(0)
-    this.t_wall = this.physics.add.staticImage(480, 30, 'player').setAlpha(0)
-    this.r_wall = this.physics.add.staticImage(985, 300, 'player').setAlpha(0)
-    this.l_wall = this.physics.add.staticImage(-25, 300, 'player').setAlpha(0)
+    this.b_wall = this.physics.add.staticImage(480, 625, 'player').setAlpha(0).setData('name', 'BotWall')
+    this.t_wall = this.physics.add.staticImage(480, 30, 'player').setAlpha(0).setData('name', 'TopWall')
+    this.r_wall = this.physics.add.staticImage(985, 300, 'player').setAlpha(0).setData('name', 'RightWall')
+    this.l_wall = this.physics.add.staticImage(-25, 300, 'player').setAlpha(0).setData('name', 'LeftWall')
 
     this.b_wall.body.setSize(960, 50)
     this.t_wall.body.setSize(960, 60)
@@ -69,47 +71,51 @@ export class Game extends Phaser.Scene {
 
     this.player_paddle.setOrigin(0)
 
-    this.comp = new Computer_paddle(this, this.ball, this.DIRECTION)
+    this.comp = new Computer_paddle(this, this.ball, this.ball.direction)
 
-    this.physics.add.collider(this.ball, [this.b_wall], () => {
-      this.ball.setVelocity(this.upMotion.x, this.upMotion.y)
-      this.ballMotion.setAlpha(0.3)
+    this.ball.ballCollider([this.b_wall, this.t_wall, this.r_wall, this.l_wall, this.player_paddle, this.comp])
 
-    })
 
-    this.physics.add.collider(this.ball, [this.t_wall], () => {
-      this.ball.setVelocity(this.downMotion.x, this.downMotion.y)
-      this.ballMotion.setAlpha(0.3)
-    })
-    this.physics.add.collider(this.ball, [this.comp], () => {
-      this.ball.setVelocity(this.leftMotion.x - 100, this.leftMotion.y * Phaser.Math.FloatBetween(-1, 1))
-      this.DIRECTION = -1;
-      this.comp.updateDir(this.DIRECTION)
-      this.ballMotion.setAlpha(1)
 
-    })
-    this.physics.add.collider(this.ball, [this.player_paddle], () => {
-      this.ball.setVelocity(this.rightMotion.x + 100, this.rightMotion.y * Phaser.Math.FloatBetween(-1, 1))
-      this.DIRECTION = 1
-      this.comp.updateDir(this.DIRECTION)
-      this.ballMotion.setAlpha(1)
-    })
+    // this.physics.add.collider(this.ball, [this.b_wall], () => {
+    //   this.ball.setVelocity(this.upMotion.x, this.upMotion.y)
+    //   this.ballMotion.setAlpha(0.3)
+    // })
 
-    this.physics.add.collider(this.ball, this.l_wall, () => {
-      this.ball.setVelocity(this.rightMotion.x, this.rightMotion.y * Phaser.Math.FloatBetween(-1, 1))
-      this.DIRECTION = 1
-      this.comp.updateDir(this.DIRECTION)
-      this.updateScore(1)
-      this.ballMotion.setAlpha(0.3)
-    })
+    // this.physics.add.collider(this.ball, [this.t_wall], () => {
+    //   this.ball.setVelocity(this.downMotion.x, this.downMotion.y)
+    //   this.ballMotion.setAlpha(0.3)
+    // })
 
-    this.physics.add.collider(this.ball, this.r_wall, () => {
-      this.ball.setVelocity(this.leftMotion.x, this.leftMotion.y * Phaser.Math.FloatBetween(-1, 1))
-      this.DIRECTION = -1;
-      this.comp.updateDir(this.DIRECTION)
-      this.updateScore(2)
-      this.ballMotion.setAlpha(0.3)
-    })
+    // this.physics.add.collider(this.ball, [this.comp], () => {
+    //   this.ball.setVelocity(this.leftMotion.x - 100, this.leftMotion.y * Phaser.Math.FloatBetween(-1, 1))
+    //   this.ball.direction = -1
+    //   this.comp.updateDir(this.ball.direction)
+    //   this.ballMotion.setAlpha(1)
+
+    // })
+    // this.physics.add.collider(this.ball, [this.player_paddle], () => {
+    //   this.ball.setVelocity(this.rightMotion.x + 100, this.rightMotion.y * Phaser.Math.FloatBetween(-1, 1))
+    //   this.ball.direction = 1
+    //   this.comp.updateDir(this.ball.direction)
+    //   this.ballMotion.setAlpha(1)
+    // })
+
+    // this.physics.add.collider(this.ball, this.l_wall, () => {
+    //   this.ball.setVelocity(this.rightMotion.x, this.rightMotion.y * Phaser.Math.FloatBetween(-1, 1))
+    //   this.ball.direction = 1
+    //   this.comp.updateDir(this.ball.direction)
+    //   this.updateScore(1)
+    //   this.ballMotion.setAlpha(0.3)
+    // })
+
+    // this.physics.add.collider(this.ball, this.r_wall, () => {
+    //   this.ball.setVelocity(this.leftMotion.x, this.leftMotion.y * Phaser.Math.FloatBetween(-1, 1))
+    //   this.ball.direction = -1
+    //   this.comp.updateDir(this.ball.direction)
+    //   this.updateScore(2)
+    //   this.ballMotion.setAlpha(0.3)
+    // })
 
     this.KEYS = this.input.keyboard?.addKeys(this.key_conf) as keyBindings
     this.scene.run('GameInterface', this).setVisible(true)
@@ -126,23 +132,26 @@ export class Game extends Phaser.Scene {
 
     })
 
-    // console.log(this.ball)
+    this.ball.colliderListeners()
 
-    this.t_wall!.body.onCollide = true
-    this.physics.world.on('collide', (object1, object2, body1, body2) => {
-      if (object2 == this.t_wall) {
-        this.ball.setVelocity(this.downMotion.x, this.downMotion.y)
-        this.ballMotion.setAlpha(0.3)
-      }
-    })
+    // this.t_wall!.body.onCollide = true
+    // this.physics.world.on('collide', (object1, object2, body1, body2) => {
+    //   if (object2 == this.t_wall) {
+    //     this.ball.setVelocity(this.downMotion.x, this.downMotion.y)
+    //     this.ballMotion.setAlpha(0.3)
+    //   }
+    // })
 
   }
+
+  
 
 
   update() {
 
-    this.upMotion = new Phaser.Math.Vector2(SPEED * this.DIRECTION, -SPEED)
-    this.downMotion = new Phaser.Math.Vector2(SPEED * this.DIRECTION, SPEED)
+
+    this.upMotion = new Phaser.Math.Vector2(SPEED * this.ball.direction, -SPEED)
+    this.downMotion = new Phaser.Math.Vector2(SPEED * this.ball.direction, SPEED)
     this.rightMotion = new Phaser.Math.Vector2(SPEED + ACELERATION, SPEED)
     this.leftMotion = new Phaser.Math.Vector2(-SPEED - ACELERATION, SPEED)
 
--- src/core/Ball.ts
+++ src/core/Ball.ts
@@ -1,7 +1,9 @@
+import { SPEED } from "../constants"
 
 
 export class Ball extends Phaser.Physics.Arcade.Image {
-  direction!: number
+  direction = 1
+  upMotion: Phaser.Math.Vector2
 
   constructor(scene: Phaser.Scene) {
     super(scene, 300, 300, 'ball')
@@ -10,12 +12,21 @@ export class Ball extends Phaser.Physics.Arcade.Image {
     this.setCircle(15)
 
     this.body!.onCollide = true
+    this.upMotion = new Phaser.Math.Vector2(SPEED * this.direction, -SPEED)
   }
   
   deltaPos = new Phaser.Math.Vector2(this.x, this.y)
 
-  ballCollider(obect_2: Phaser.GameObjects.GameObject, callback: Phaser.Types.Physics.Arcade.ArcadePhysicsCallback) {
-    this.scene.physics.add.collider(this, obect_2, callback, () => true, this.scene)
+  ballCollider(object2: Phaser.GameObjects.GameObject | Phaser.GameObjects.GameObject[]){
+    this.scene.physics.add.collider(this, object2, () => true, ()=>true, this.scene)
+  }
+
+  colliderListeners() {
+    this.scene.physics.world.on('collide', (obj1, obj2) => {
+      if (obj2.getData('name') == 'BotWall') {
+        console.log('Bot wall hit')
+      }
+    })
   }
   
 }
